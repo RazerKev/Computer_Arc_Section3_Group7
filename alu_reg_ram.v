07ac3c54318316e92318016e001d5a4cc19a2db2
Nando827 <78919495+Nando827@users.noreply.github.com>
2021-03-26 19:20:14 -0400

    Add files via upload

--- alu_reg_ram.v
+++ alu_reg_ram.v
@@ -1,10 +1,11 @@
-module alu_reg_ram ( clock, write, reset, writeReg, data, readA, readB, sel, muxSel, cin, writeRam, ramOut, Cout, status, aluOut);
+module alu_reg_ram ( READA, READB, clock, write, reset, writeReg, data, readA, readB, sel, muxSel, cin, writeRam, ramOut, Cout, status, aluOut);
 input clock;
 input write; 
 input reset; 
 input [4:0] writeReg; 
 input [63:0] data; 
-input [4:0] readA, readB;
+input [4:0] readA;
+input [4:0] readB;
 input [4:0] sel; 
 input muxSel; 
 input cin;
@@ -14,18 +15,21 @@ output [63:0] ramOut;
 output Cout; 
 output [3:0] status;
 output [63:0] aluOut;  
-
+output [63:0]READA;
+output [63:0]READB;
 
 wire [63:0] A_out, B_out, mux_2_1_out, alu_out, mux2_to_1_out; 
 assign aluOut = alu_out; 
+assign READA= A_out;
+assign READB= B_out;
 
 regfile_32x64 u1  ( clock, write, reset, writeReg, data, readA, A_out, readB, B_out); 
 mux_Aout_2to1 u2  ( A_out, writeReg, muxSel,  mux2_to_1_out);
 aluFile 		  u3	( mux2_to_1_out, B_out, cin, sel, alu_out, Cout, status);  
-RAM256x64     u4  (alu_out[7:0], clock, A_out, writeRam, ramOut);
+ram_test      u4  (alu_out[7:0], clock, A_out, writeRam, ramOut);
 endmodule 
 
-module mux_Aout_2to1 (A_reg_out, Writeregister , Sel, out);//writeresgister is 4bit, and RegAout is 64 bit
+module mux_Aout_2to1 (A_reg_out, Writeregister , Sel, out);
 
 input [63:0] A_reg_out;
 input [4:0]Writeregister; 
@@ -34,13 +38,6 @@ output wire [63:0] out;
 
 assign out = Sel == 0 ? A_reg_out:
              Sel == 1 ? Writeregister : 1'bx;
-//always @ (A_reg_out or Writeregister or Sel) 
-//    begin 
-//        case (Sel) 
-//         0: out = A_reg_out;
-//         1: out = Writeregister; 
-//         default: out = 1'bx;
-//        endcase
-//    end
+  
 	 
 endmodule 
--- alu_reg_ram_testbench.v
+++ alu_reg_ram_testbench.v
@@ -1,73 +1,96 @@
 `timescale 1ns / 1ns
-module alu_reg_ram_testbench(); 
-
-//Inputs
+module alu_reg_ram_testbench ();
 reg clock;
 reg write; 
 reg reset; 
-reg [4:0] writeReg;
+reg [4:0] writeReg; 
 reg [63:0] data; 
 reg [4:0] readA;
 reg [4:0] readB;
-reg [4:0] sel;
+reg [4:0] sel; 
 reg muxSel; 
 reg cin;
-reg writeRam;
-
-//Outputs
+reg writeRam; 
 wire [63:0] ramOut; 
 wire Cout; 
 wire [3:0] status;
-wire [63:0] aluOut; 
+wire [63:0] aluOut;  
+wire [63:0]READA;
+wire [63:0]READB;
 
-//ramFile dut (.clock(clock), .write(write), .reset(reset), .writeReg(writeReg), .data(data), .readA(readA), .readB(readB), .sel(sel), .muxSel(muxSel), .cin(cin), .writeRam(writeRam), .ramOut(ramOut), .Cout(Cout), status(status), .aluOut(aluOut));
+alu_reg_ram dut(READA,READB,clock,write, reset, writeReg, data, readA, readB, sel,muxSel, cin, writeRam, ramOut, Cout, status, aluOut); 
 
-alu_reg_ram dut (clock, write, reset, writeReg, data, readA, readB, sel, muxSel, cin, writeRam, ramOut, Cout, status, aluOut);
+initial
 
-initial begin 
+    begin
+        reset     <=   1'b1; 
+        clock     <=   1'b1;
+        write     <=   1'b1;
+        data      <=   64'b0;
+        readA     <=   5'd30;                          
+        readB     <=   5'd29;  
+        #20 reset   <=   1'b0;
+        #320 write  <=   1'b0;
+		  #320 $stop; 		  
+    end
+	 
+always #10 clock <= ~clock;
 
-//$monitor("sel=%d clock=%d, write=%d, reset=%d, writeReg=%d, data=%d, readA=%d, readB=%d, sel=%d, muxSel=%d, cin=%d, writeRam=%d, ramOut=%d, Cout=%d, status=%d, aluOut=%d", 
-//clock, write, reset, writeReg, data, readA, readB, sel, muxSel, cin, writeRam, ramOut, Cout, status, aluOut);
-
-clock <=  1'b1; 
-write <= 1'b1;
-reset <= 1'b1;
-data <= 64'b0; 
-
-readA <= 5'd30; 
-readB <= 5'd29; 
-#5 reset <= 1'b0; 
-#320 write <= 1'b0; 
-  
-end
-
-always  
-	
-	#5 clock <= ~clock; 
-
-always begin
-	#5 data <= {$random, $random}; 
-	writeReg <= writeReg + 5'b1; 
-	readA <= readA + 5'b1; 
-	readB <= readB + 5'b1; 
-	#5; 
-end
-
+	always begin
+	     #10// load in data into regfile, write it to reg 29 
+		  data <= 64'd14;
+		  writeReg <= 5'd29;
+		  #10
+        data <= 64'd14;
+		  writeReg <= 5'd30; // load in the same value onto reg 30
+        #10
+		  write <= 1'b0; //disable write, the value in regfile will not write
+		  #10// now we have to select what goes into alu on a side, operation, and enable writeram 
+		  muxSel <= 1'b0; 
+		  sel <= 5'b10000;//a+b
+		  cin <= 1'b0;
+		  writeRam = 1'b1; 
+		  #10 //a-b
+		  muxSel <= 1'b0; 
+		  sel <= 5'b10010;
+		  cin <= 1'b1;
+		  writeRam = 1'b1;
+		  #10 // shift right 
+		  muxSel <= 1'b0; 
+		  sel <= 5'b10100;
+		  cin <= 1'b0;
+		  writeRam = 1'b1;
+		 
+        //#20 reset   <=   1'b0;
+        //#320 write  <=   1'b0;
+        
+    end  
+    
+/*always 
+begin
+    
+        #5 data <= {$random,$random};
+        writeReg   <=       writeReg + 5'b1;
+        readA <= readA+5'b1;
+        readB <= readB+5'b1;
+        #5;
+        end 
 always 
-	begin 
-		write <= 1'b1;
-		writeRam <= 1'b1;
-		readA <= 1'b1;
-		readB <= 1'b1;
-		writeReg <= 5'b00110;
-		reset <= 1'b0;
-		clock <= 1'b1;
-		sel <= 5'b10000; 
-		cin <=  1'b0;
-		muxSel <= 1'b0;
-		//writeReg <=1'b01100;
-		#5 
-		#500 $stop; 
-	end
+    begin 
+        write      <=      1'b1;                               
+        writeRam   <=      1'b1;                               
+        readA      <=      5'b1;                           
+        readB      <=      5'b1;                       
+        writeReg   <=      5'b00110;                           
+        reset      <=      1'b0;
+        clock      <=      1'b1;
+        sel        <=      5'b10000;                       //A+B
+        cin        <=      1'b0;
+        muxSel     <=      1'b0;                   
+        
+        //writeregister     <=       5'b00010;
+        #5;
+        #500 $stop;
+    end
+*/
 endmodule
-
